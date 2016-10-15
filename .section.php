394cc2f80f04ebca5d6b24cb0455ea74f7386668
Anna Shergina <anik.97@bk.ru>
2016-10-15 20:28:31 +0300

    git-repository creation

---
+++ .section.php
@@ -0,0 +1,8 @@
+<?
+$sSectionName = "Главная страница";
+$arDirProperties = array(
+   "description" => "",
+   "keywords" => "Детская одежда, детская обувь, детское питание, игрушки и т.д.",
+   "robots" => "index, follow"
+);
+?>
