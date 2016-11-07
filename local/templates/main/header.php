ee5b8ca35f8b350152315bc293e9f3897fa4340c
Anna Shergina <anik.97@bk.ru>
2016-11-07 23:42:44 +0300

    Template of top menu component has been changed

--- local/templates/main/header.php
+++ local/templates/main/header.php
@@ -53,15 +53,23 @@ Loc::loadMessages(__FILE__);?>
             <div id="site_title">
                 <h1><a href="#">For Kids</a></h1>
             </div>
-            <div id="tooplate_menu">
-                <ul>
-                    <li><a href="index.html" class="current">Home</a></li>
-                    <li><a href="about.html">About Us</a></li>
-                    <li><a href="blog.html">Blog</a></li>
-                    <li><a href="gallery.html">Gallery</a></li>
-                    <li><a href="contact.html">Contact</a></li>
-                </ul>
-            </div> <!-- end of tooplate_menu -->
+            <?$APPLICATION->IncludeComponent("bitrix:menu", "top_menu", Array(
+	"ALLOW_MULTI_SELECT" => "N",	// Разрешить несколько активных пунктов одновременно
+		"CHILD_MENU_TYPE" => "left",	// Тип меню для остальных уровней
+		"DELAY" => "N",	// Откладывать выполнение шаблона меню
+		"MAX_LEVEL" => "2",	// Уровень вложенности меню
+		"MENU_CACHE_GET_VARS" => array(	// Значимые переменные запроса
+			0 => "",
+		),
+		"MENU_CACHE_TIME" => "3600",	// Время кеширования (сек.)
+		"MENU_CACHE_TYPE" => "A",	// Тип кеширования
+		"MENU_CACHE_USE_GROUPS" => "Y",	// Учитывать права доступа
+		"ROOT_MENU_TYPE" => "left",	// Тип меню для первого уровня
+		"USE_EXT" => "Y",	// Подключать файлы с именами вида .тип_меню.menu_ext.php
+	),
+	false
+);?>
+
         </div> <!-- end of forever header -->
         <div id="tooplate_main_top"></div>
         <div id="tooplate_middle">
